d573e3a4b87d55ed3a8e8238f597d7549fef5639
cult2rologist <aw6x4klb@gmail.com>
2024-09-08 18:25:38 -0300

    getting resource toml

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,7 +3,17 @@ from app import app
 from app.conjugate import Conjugador
 from app.utils.form import form
 from app.srv.verb import get_conjugations
-import json
+from tomllib import load
+import sys
+import os
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = current_dir[:current_dir.rfind('\\')]
+desc_file = os.path.join(parent_dir, 'resources/description.toml')
+
+with open(desc_file, 'rb') as f:
+    data = load(f)
 
 @app.route('/')
 def index():
@@ -13,4 +23,4 @@ def index():
 @form.params(form=lambda: request.form, validators={"verb": Conjugador.is_spanish_verb})
 def result(verb, region):
     verb_info, conjugations = get_conjugations(verb, region)
-    return render_template('result.html', verb=verb_info, conjugations=conjugations)
+    return render_template('result.html', verb=verb_info, conjugations=conjugations, description=data)
