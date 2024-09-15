cddaa048064bc37c9f7ca3d6c46445f89daa182b
cult2rologist <aw6x4klb@gmail.com>
2024-09-14 23:10:11 -0300

    changed style a bit

--- app/utils/form.py
+++ app/utils/form.py
@@ -1,16 +1,19 @@
 import inspect
 import re
 
+
 def sanitize_input(value):
     """
     Delete HTML-tags
     """
-    return re.sub(r'<[^>]*?>', '', value)
+    return re.sub(r"<[^>]*?>", "", value)
+
 
 class Form:
     def params(self, form, validators):
         def factory(fn):
             names = inspect.getfullargspec(fn).args
+
             def decorator(*args, **kwargs):
                 form_data = form()
                 for name in names:
@@ -19,7 +22,10 @@ class Form:
                         validators[name](form_data_value)
                     kwargs[name] = form_data[name].strip()
                 return fn(*args, **kwargs)
+
             return decorator
+
         return factory
+
 
 form = Form()
