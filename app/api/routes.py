f14d2c5dad92460f91a629bbcb7ee5b9df329bc2
cult2rologist <aw6x4klb@gmail.com>
2024-09-21 22:26:15 -0300

    added new source

--- app/api/routes.py
+++ app/api/routes.py
@@ -10,19 +10,30 @@ from flask import render_template, request
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
-desc_file = os.path.join(parent_dir, 'resources', 'description.toml')
+desc_file = os.path.join(parent_dir, "resources", "description.toml")
+exp_file = os.path.join(parent_dir, "resources", "explanation_table.toml")
+
+with open(exp_file, "rb") as exp:
+    data_exp = load(exp)
+
+with open(desc_file, "rb") as desc:
+    data_desc = load(desc)
 
 
-with open(desc_file, 'rb') as f:
-    data = load(f)
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/result', methods=['POST'])
+
+@app.route("/result", methods=["POST"])
 @form.params(form=lambda: request.form, validators={"verb": Conjugador.is_spanish_verb})
 def result(verb, region):
     verb_info, conjugations = get_conjugations(verb, region)
-    return render_template('result.html', verb=verb_info, conjugations=conjugations, 
-                                          description=data, region=region)
+    return render_template(
+        "result.html",
+        verb=verb_info,
+        conjugations=conjugations,
+        explanation=data_exp,
+        description=data_desc,
+        region=region,
+    )
